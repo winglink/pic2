eae4581213264546190cdfa22aefcbc18b18a1ab
ww <zyq00123@icloud.com>
2020-06-27 12:03:09 +0800

    complete 8 jpg

--- main.cpp
+++ main.cpp
@@ -1,28 +1,32 @@
 #include <iostream>
 #include "showpic.h"
+#include <string>
 int main() {
     std::vector<Tx*> tx;
-    Mat  pic = Mat(300,300,CV_8UC1,Scalar(0));
+//    Mat  pic = Mat(300,300,CV_8UC1,Scalar(0));
     tx.push_back(new Square);
     tx.push_back(new Circle);
     tx.push_back(new Wbx);
+    tx.push_back(new Wjx);
+    tx.push_back(new Sx);
     tx.push_back(new Halfcircle);
     tx.push_back(new Ttx);
     tx.push_back(new Triangle);
     Point pp[] = {Point(75,75),Point(225,75),Point(225,225),Point(75,225),Point(150,75),Point(150,225)};
     std::vector<Scalar> scalar;
-    for(int i = 0;i < 6;i++){
-        scalar.push_back(Scalar(125 + 15 * i));
+    for(int i = 0;i < 8;i++){
+        scalar.push_back(Scalar(125 + 10 * i));
     }
     srand((unsigned )time(NULL));
     int index(0);
     for(int i = 0;i < tx.size();i++){
+        Mat  pic = Mat(300,300,CV_8UC1,Scalar(0));
         tx[i]->show(pic,pp[4],scalar[i]);
-
+        std::string  ouputname = std::to_string(i + 1) + ".jpg";
+        imwrite(ouputname,pic);
     }
 
-    imwrite("out.jpg",pic);
-    waitKey();
+//    waitKey();
     for(auto tmp : tx){
         delete  tmp;
     }
--- showpic.h
+++ showpic.h
@@ -57,7 +57,7 @@ public:
     }
 
 private:
-        int ll = 10;
+        int ll = 8;
 };
 class Square : public Tx{
 public:
@@ -125,14 +125,15 @@ private:
 };
 class Ttx : public Tx{
 public:
-    Ttx(int h) : h(h) {}
+    Ttx(int h, int a, int b) : h(h), a(a), b(b) {}
     Ttx(){}
 
     void show(Mat &mat, const Point &center, const Scalar &scalar) override {
-        input.push_back(Point(center.x - this->h / 2.0,center.y - this->h /2.0));
-        input.push_back(Point(center.x + this->h / 2.0,center.y - this->h /2.0));
-        input.push_back(Point(center.x + this->h / 2.0 + this->h,center.y + this->h /2.0));
-        input.push_back(Point(center.x -  this->h / 2.0 - this->h,center.y + this->h /2.0));
+        int tt = (a + 2 * b) * h / (3.0 * (a + b));
+        input.push_back(Point(center.x - a / 2.0,center.y - tt));
+        input.push_back(Point(center.x + a / 2.0,center.y - tt));
+        input.push_back(Point(center.x + b / 2.0,center.y + h - tt));
+        input.push_back(Point(center.x - b / 2.0,center.y + h - tt));
 
 //        for(auto point : input){
 //            std::cout << point.x << " " << point.y << std::endl;
@@ -142,10 +143,11 @@ public:
     }
 
     void show_py(Mat &mat, const Point &center, const Scalar &scalar) override {
-        input.push_back(Point(center.x - this->h / 2.0,center.y - this->h /2.0));
-        input.push_back(Point(center.x + this->h / 2.0,center.y - this->h /2.0));
-        input.push_back(Point(center.x + this->h / 2.0 + this->h,center.y + this->h /2.0));
-        input.push_back(Point(center.x -  this->h / 2.0 - this->h,center.y + this->h /2.0));
+        int tt = (a + 2 * b) * h / (3.0 * (a + b));
+        input.push_back(Point(center.x - a / 2.0,center.y - tt));
+        input.push_back(Point(center.x + a / 2.0,center.y - tt));
+        input.push_back(Point(center.x + b / 2.0,center.y + h - tt));
+        input.push_back(Point(center.x - b / 2.0,center.y + h - tt));
         int py(0) ;
         if(center.y >= 150)
             py = 150 - input[0].y ;
@@ -162,7 +164,9 @@ public:
     }
 
 private:
-    int h = 20;
+    int h = 17;
+    int a = 15;
+    int b = 30;
 };
 class Circle : public Tx{
 public:
@@ -194,7 +198,8 @@ public:
     Halfcircle(int r, int initan, int endan) : r(r), initan(initan), endan(endan) {}
 
     void show(Mat &mat, const Point &center, const Scalar &scalar) override {
-        ellipse(mat,center,Size(r,r),0,initan,endan,scalar,-1);
+        Point center2(center.x,center.y + 4 * r / (3 * PI));
+        ellipse(mat,center2,Size(r,r),0,initan,endan,scalar,-1);
     }
 
     void show_py(Mat &mat, const Point &center, const Scalar &scalar) override {
@@ -203,8 +208,69 @@ public:
 
 
 private:
-    int r = 45;
+    int r = 25;
     int initan = 0;
     int endan = -180;
+};
+
+class Sx : public Tx{
+public:
+    Sx() {}
+    Sx(int r, int initan, int endan) : r(r), initan(initan), endan(endan) {}
+
+    void show(Mat &mat, const Point &center, const Scalar &scalar) override {
+        Point center2(center.x,center.y + 4 * r / (3 * PI));
+        ellipse(mat,center2,Size(r,r),0,initan,endan,scalar,-1);
+
+    }
+
+    void show_py(Mat &mat, const Point &center, const Scalar &scalar) override {
+
+    }
+
+private:
+    int r = 40;
+    int initan = -30;
+    int endan = -150;
+};
+class Wjx : public Tx {
+public:
+    Wjx() {}
+    Wjx(int r, int r1) : r(r), R(r1) {}
+
+    void show(Mat &mat, const Point &center, const Scalar &scalar) override {
+        std::vector<Point> input1;
+        std::vector<Point> input2;
+        Point tmp;
+        for(int i = 0;i < 5;i++){
+            tmp.x = center.x + cos((18 + 72 * i) / 180.0 * PI) * R;
+            tmp.y = center.y - sin((18 + 72 * i) / 180.0 * PI) * R;
+            input1.push_back(tmp);
+        }
+
+        for(int i = 0;i < 5;i++){
+            tmp.x = center.x + cos((54+ 72 * i) / 180.0 * PI) * r;
+            tmp.y = center.y - sin((54+ 72 * i) / 180.0 * PI) * r;
+            input2.push_back(tmp);
+        }
+        auto start = input2.begin();
+        for(auto init = input1.begin(); init != input1.end();init++){
+             start = input2.insert(start,(*init));
+             start++;
+             if( (start + 1) != input2.end())
+                 start++;
+        }
+        std::vector<std::vector<Point>> result;
+        result.push_back(input2);
+        fillPoly(mat,result,scalar);
+    }
+
+    void show_py(Mat &mat, const Point &center, const Scalar &scalar) override {
+
+    }
+
+private:
+    int r = 30;
+    int R = 65;
 };
 #endif //PIC2_SHOWPIC_H
